e2b1646393e1f8b867e6390f3875fbb993106d0b
Esteban Arias <earias@i4digital.com>
2023-09-05 07:59:28 -0500

    improve rendering

--- src/features/accountStatement/accountStatementRoutes.tsx
+++ src/features/accountStatement/accountStatementRoutes.tsx
@@ -5,27 +5,13 @@ import CreateAccountStatement from "./pages/CreateAccountStatement";
 import DetailAccountStatement from "./pages/DetailAccountStatement";
 import EditAccountStatement from "./pages/EditAccountStatement";
 
-const AccountStatementRoutes = () => {
-  // const CreateAccountStatement = lazy(
-  //   () => import("./pages/CreateAccountStatement")
-  // );
-  // const ConsultAccountStatement = lazy(
-  //   () => import("./pages/ConsultAccountStatement")
-  // );
-  // const DetailAccountStatement = lazy(
-  //   () => import("./pages/DetailAccountStatement")
-  // );
-  // const EditAccountStatement = lazy(
-  //   () => import("./pages/EditAccountStatement")
-  // );
-  return (
-    <Routes>
-      <Route path="/crear" element={<CreateAccountStatement />} />
-      <Route path="/consultar" element={<ConsultAccountStatement />} />
-      <Route path="/detalle/:id" element={<DetailAccountStatement />} />
-      <Route path="/editar/:id" element={<EditAccountStatement />} />
-    </Routes>
-  );
-};
+const AccountStatementRoutes = () => (
+  <Routes>
+    <Route path="/crear" element={<CreateAccountStatement />} />
+    <Route path="/consultar" element={<ConsultAccountStatement />} />
+    <Route path="/detalle/:id" element={<DetailAccountStatement />} />
+    <Route path="/editar/:id" element={<EditAccountStatement />} />
+  </Routes>
+);
 
 export default memo(AccountStatementRoutes);
