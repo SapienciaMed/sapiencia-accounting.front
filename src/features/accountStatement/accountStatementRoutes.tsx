892c0dfd4e06ba492686f8bba1adfd59dc032418
Esteban Arias <earias@i4digital.com>
2023-10-09 10:10:07 -0500

    add example to mange private routes

--- src/features/accountStatement/accountStatementRoutes.tsx
+++ src/features/accountStatement/accountStatementRoutes.tsx
@@ -1,5 +1,6 @@
 import { memo } from "react";
 import { Route, Routes } from "react-router-dom";
+import PrivateRoute from "../../common/components/Guard/auth-private-guard";
 import ConsultAccountStatement from "./pages/ConsultAccountStatement";
 import CreateAccountStatement from "./pages/CreateAccountStatement";
 import DetailAccountStatement from "./pages/DetailAccountStatement";
@@ -9,7 +10,15 @@ import TracingAccountStatement from "./pages/TracingAccountStatement";
 
 const AccountStatementRoutes = () => (
   <Routes>
-    <Route path="/crear" element={<CreateAccountStatement />} />
+    <Route
+      path="/crear"
+      element={
+        <PrivateRoute
+          element={<CreateAccountStatement />}
+          allowedAction="CUENTA_COBRO_CREAR"
+        />
+      }
+    />
     <Route path="/consultar" element={<ConsultAccountStatement />} />
     <Route path="/detalle/:id" element={<DetailAccountStatement />} />
     <Route path="/editar/:id" element={<EditAccountStatement />} />
