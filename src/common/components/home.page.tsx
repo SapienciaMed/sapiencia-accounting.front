e57ef092bed998e23f9f607994a458d74f60698b
carlos <cmmontoya@i4digital.com>
2023-11-22 13:29:06 -0500

    feature home welcome

--- src/common/components/home.page.tsx
+++ src/common/components/home.page.tsx
@@ -1,7 +1,20 @@
 import * as React from "react";
+import ContabilidadIcon from "../../../src/public/images/icons/application-icon-9.png";
 
 function HomePage(): React.JSX.Element {
-  return <div> Home Contabilidad !!!!</div>;
+  return (
+    <div className="dashboard-margin" style={{ justifyContent: "center" }}>
+      <section className="welcome-container">
+        <span className="text-dasboard huge text-center">Bienvenid@ al </span>
+      </section>
+      <div className="display-justify-flex-center home-aplications">
+        <img src={ContabilidadIcon} alt="icono-contabilidad" />
+      </div>
+      <span className="text-dasboard large text-center">
+        Módulo de contabilidad
+      </span>
+    </div>
+  );
 }
 
 export default HomePage;
