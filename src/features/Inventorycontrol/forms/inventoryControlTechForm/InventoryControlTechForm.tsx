076d1ecb92fb3114f5c91712791e9bb4089b99d2
carlos <cmmontoya@i4digital.com>
2023-11-29 13:49:54 -0500

    titles change the inventory

--- src/features/Inventorycontrol/forms/inventoryControlTechForm/InventoryControlTechForm.tsx
+++ src/features/Inventorycontrol/forms/inventoryControlTechForm/InventoryControlTechForm.tsx
@@ -34,7 +34,7 @@ const InventoryControlTechForm = ({
       <div className=" container-sections-forms ml-20px mr-20px">
         <div className="pb-40px">
           <span className="text-black huge bold grid-span-4-columns">
-            Control de inventario activos tecnológicos
+            Histórico inventario activo tecnológicos
           </span>
 
           <div className="button-save-container-display mr-33px mt--18px button-create-business">
