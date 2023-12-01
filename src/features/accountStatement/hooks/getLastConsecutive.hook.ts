0632979519f235c460aa4e271d27ea93abf54c03
carlos <cmmontoya@i4digital.com>
2023-12-01 13:07:45 -0500

    bug resolve

--- src/features/accountStatement/hooks/getLastConsecutive.hook.ts
+++ src/features/accountStatement/hooks/getLastConsecutive.hook.ts
@@ -3,6 +3,7 @@ import useCrudService from "../../../common/hooks/crud-service.hook";
 import { IAccountStatement } from "../../../common/interfaces/accountStatement.interface";
 import { ApiResponse } from "../../../common/utils/api-response";
 import { urlApiAccounting } from "../../../common/utils/base-url";
+import { EResponseCodes } from "../../../common/constants/api.enum";
 
 export const useGetLastConsecutive = () => {
   const { get } = useCrudService(urlApiAccounting);
@@ -13,6 +14,9 @@ export const useGetLastConsecutive = () => {
     try {
       const endpoint = "/api/v1/account-statement/get-last";
       const resp: ApiResponse<IAccountStatement> = await get(endpoint);
+      if (resp.operation.code === EResponseCodes.FAIL) {
+        return setLastConsecutive("0");
+      }
       const consecutiveId = String(resp.data.accountNum + 1);
       setLastConsecutive(consecutiveId);
     } catch (err) {
