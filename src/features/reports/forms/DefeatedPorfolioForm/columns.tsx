b764d60beb4cc99aa8e63de65d57c62a49d7a8d5
Esteban Arias <earias@i4digital.com>
2023-12-01 16:24:49 -0500

    add days format on defeated portfolio report

--- src/features/reports/forms/DefeatedPorfolioForm/columns.tsx
+++ src/features/reports/forms/DefeatedPorfolioForm/columns.tsx
@@ -22,16 +22,14 @@ export const tableColumns: ITableElement<IDefeatedTable>[] = [
   {
     fieldName: "accountStatement.expirationDate",
     header: "Fecha de vencimiento",
-    renderCell: () => <>{DateTime.now().toFormat("yyyy/MM/dd")}</>,
   },
 
   {
     fieldName: "expiratioDays",
     header: "Días de vencimiento",
     renderCell: (el) => {
-      moment.locale("es");
       return (
-        <>{moment(el.accountStatement.expirationDate.toString()).fromNow()}</>
+        <>{moment().diff(el.accountStatement.expirationDate, "days").toString()}</>
       );
     },
   },
